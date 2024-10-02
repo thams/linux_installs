f1afda84dcabded2d55df2b682b15a67aec2be24
Kurt Thams <thams@thams.com>
2024-10-02 14:55:36 -0700

    Create docker.sh
    first

---
+++ docker.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Install Docker and Docker Compose on Ubuntu
+
+# Set up logging for the user data script in EC2 instances.
+# All output is saved to /var/log/user-data.log and sent to syslog and to console
+# This line is particularly useful in EC2 user data scripts because:
+# It helps in debugging issues with instance initialization.
+sudo exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
+
+sudo -u ubuntu -i <<'EOF2'
+
+
+# Add Docker's official GPG key:
+sudo apt-get update -y
+sudo apt-get install -y ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor | sudo tee /etc/apt/keyrings/docker.gpg > /dev/null          
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update -y
+
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Optional: Prove it works.
+# sudo docker run hello-world
